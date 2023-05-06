8a155cc7393e7c531ed57a787d2bbb4ba50bc6e2
profitrise <marketing_olga@ukr.net>
2023-05-06 08:45:18 +0200

    correct

--- src/route/index.js
+++ src/route/index.js
@@ -369,7 +369,10 @@ router.get('/bio', function (req, res) {
         'Leonardo designed and invented a wide variety of machines and devices, including a helicopter, a diving suit, and a self-propelled cart. Many of his inventions were centuries ahead of their time.',
       year: 'Late 15th to early 16th century',
       field: 'Invention',
-=======
+    },
+  })
+})
+
 router.get('/person', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('person', {
@@ -464,7 +467,6 @@ router.get('/person', function (req, res) {
           ],
         },
       ],
-
     },
   })
 })
